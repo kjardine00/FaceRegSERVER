d6bbbd3681034f8ecdaead36b3e203329c474d38
Kyle Jardine <kjardine00@gmail.com>
2023-12-28 16:22:22 -0500

    adding env variables

--- server.js
+++ server.js
@@ -12,11 +12,13 @@ const image = require("./controllers/image.js");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+    host: process.env.DATABASE_HOST,
     port: 5432,
-    user: "postgres",
-    password: "test",
-    database: "smart-brain",
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PW,
+    database: process.env.DATABASE_DB,
   },
 });
 
@@ -37,6 +39,6 @@ app.put("/image", (req, res) => { image.handleImage(req, res, db) });
 
 app.post('/imageurl', (req, res) => { image.handleAPIcall(req, res) })
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT, () => {
   console.log(`app is running on port ${process.env.PORT}.`);
 });
